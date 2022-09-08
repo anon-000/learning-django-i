6d52cbb90dc24654367047f4c8eb21e7b18a0a47
auro <aurosmruti.smartters@gmail.com>
2022-09-09 01:17:31 +0530

    demo api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Demo API View"""
+
+    def get(self, request, format=None):
+        """Returns list of API views features"""
+
+        api_view = [
+            'This have many mehtods like get, put , patch, post, delete',
+            'Is similar to django view',
+            'gives you superpower of customisaation',
+            'mapped manually with urls'
+        ]
+
+        return Response({'message': 'Hello!', 'api_view': api_view})
+        
+
