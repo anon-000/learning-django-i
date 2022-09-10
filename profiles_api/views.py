1d0b1c976c84b8ed862f76bf76b02920fa3e7790
auro <aurosmruti.smartters@gmail.com>
2022-09-11 01:04:48 +0530

    all methods added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
+from email import message
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloAPIView(APIView):
     """Demo API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns list of API views features"""
@@ -15,6 +20,35 @@ class HelloAPIView(APIView):
             'mapped manually with urls'
         ]
 
-        return Response({'message': 'Hello!', 'api_view': api_view})
-        
+        return Response({'message': 'Hello!','api_view':api_view})
+
+    def post(self, request, format=None):
+        """Create a  hello msgg"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({"message": message})
+        else:
+            return Response(
+                serializer.errors, 
+                status = status.HTTP_400_BAD_REQUEST,
+            )
+     
+    def put(self, request, pk=None):
+        """Create a  hello msgg"""
+        return Response({"method": "PUT"})
+    
+    def patch(self, request, pk=None):
+        """Create a  hello msgg"""
+        return Response({"method": "PATCH"})
+
+    
+    def delete(self, request, pk=None):
+        """Create a  hello msgg"""
+        return Response({"method": "DELETE"})
+
+
+
 
