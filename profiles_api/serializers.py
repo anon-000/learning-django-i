0d626b61e4f4c908e2e5bc2d3c2771368adc22f1
auro <000computricks@gmail.com>
2022-09-13 01:17:05 +0530

    wip : profile

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -4,3 +4,10 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Serializes a name filed """
     name = serializers.CharField(max_length=10)
+
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+    
